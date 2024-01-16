8cf5467a7b981bc394d5024c4ca7f9b88ad56877
Jonathan Ikpen <jonathanikpen4@gmail.com>
2024-01-16 22:10:26 +0100

    fix: cors

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -74,14 +74,15 @@ export async function POST(req: NextRequest) {
                 }
             });
             return NextResponse.json({
+                data: createdChildUser
+            }, {
                 status: 200,
                 headers: {
                     'Access-Control-Allow-Origin': '*',
                     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
                     'Access-Control-Allow-Headers': 'Content-Type',
-                },
-                data: createdChildUser
-            });
+                }
+            })
         }
 
         if(adult) {
